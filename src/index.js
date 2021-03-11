f1d354e34aa30fd54657349dc99149542ad4399d
Nikolay13-11 <mikola13.11@mail.ru>
2021-03-11 21:06:54 +0300

    End of Task

--- src/index.js
+++ src/index.js
@@ -1,12 +1,34 @@
-
 exports.min = function min (array) {
+/*if (array.length === 0 && Array.isArray(array) == false ) {
+  return 0; 
+}else {
+  return Math.min.apply(null, array);
+}*/
+
+  
+if (array == undefined || array == null || array.length == 0) {
   return 0;
+} else {
+  return Math.min.apply(null, array);
+}
 }
 
+
 exports.max = function max (array) {
-  return 0;
+  if (array == undefined || array == null || array.length == 0) {
+    return 0; 
+  }else {
+    return Math.max.apply(null, array);
+}
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  if (array == undefined || array == null || array.length == 0) {
+    return 0; 
+} else{
+   //var result = (arr.reduce((sum, current) => sum + current, 0)) / array.length;
+   //return result;
+   let result = (array.reduce((sum, current) => sum + current, 0)) / array.length;
+   return result;
+}
 }
